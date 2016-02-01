98a9cf45794da46a1176c64e36b6df8c1f6b8e31
Tyler Brockett <tylerbrockett@gmail.com>
2016-01-31 23:01:11 -0700

    HW 1 ready for submission

--- hw-1/hw-1/hw-1/ViewController.swift
+++ hw-1/hw-1/hw-1/ViewController.swift
@@ -1,10 +1,30 @@
-//
-//  ViewController.swift
-//  hw-1
-//
-//  Created by Tyler Brockett on 1/27/16.
-//  Copyright © 2016 Tyler Brockett. All rights reserved.
-//
+/*
+ * @author Tyler Brockett
+ * @project CSE 394 - Homework 1
+ * @version January 30, 2016
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Tyler Brockett
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 import UIKit
 
@@ -41,11 +61,7 @@ class ViewController: UIViewController {
             }
         }
         else{
-            let alert = UIAlertView()
-            alert.title = "Error"
-            alert.message = "There was an error gathering your daily data. Make sure you typed everything in correctly."
-            alert.addButtonWithTitle("Okay")
-            alert.show()
+            showError()
         }
     }
     
@@ -144,6 +160,16 @@ class ViewController: UIViewController {
         self.wCaloriesBurned.resignFirstResponder()
         self.weightChange.resignFirstResponder()
         return true
+    }
+    
+    func showError() {
+        /* 
+        I had an old style alert, but it was deprecated, so I found out the new method here:
+        http://stackoverflow.com/questions/30933965/how-do-i-migrate-from-uialertview-deprecated-in-ios8
+        */
+        let alert = UIAlertController(title: "Error", message:"There was an error gathering your daily data. Make sure you typed everything in correctly. All fields must be integers.", preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "Okay", style: .Default) { _ in })
+        self.presentViewController(alert, animated: true){}
     }
 }
 
